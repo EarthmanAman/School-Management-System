c3d3869572bbfb03dd28fe62bdda187d2408d4a0
EarthmanAman <hashimathman@gmail.com>
2020-05-23 22:11:13 +0300

    main models

--- main/models.py
+++ main/models.py
@@ -11,6 +11,10 @@ class Grade(models.Model):
 			* A model to store the basic description
 			* Can be linked with other database
 
+		Variables
+		-----------
+			* _GRADES = a tuple containing grades
+
 		Fields
 		-------
 			* name - to store the name of thegrade
@@ -47,8 +51,8 @@ class Pupil(models.Model):
 
 		Variables
 		----------
-			* Gender = contains tuple of genders
-			*religions = contain tuples of religions
+			* _GENDERS = contains tuple of genders
+			* _RELIGIONS = contain tuples of religions
 
 		Fields
 		--------
@@ -71,13 +75,13 @@ class Pupil(models.Model):
 
 
 	# Variables
-	GENDER = (
+	_GENDERS = (
 			("m", "Male"),
 			("f", "Female"),
 			("o", "Others"),
 		)
 
-	RELIGION = (
+	_RELIGIONS = (
 			("atheists", "Atheists"), ("baha'i", "Baha'i"), ("buddhism", "Buddhism"),
 			("christianity", "Christianity"), ("hinduism", "Hinduism"), ("islam", "Islam"),
 			("jainism", "Jainism"), ("judaism", "Judaism"), ("shintoism", "Shintoism"),
@@ -97,10 +101,100 @@ class Pupil(models.Model):
 
 	dob 		= models.DateField()
 	nationality = models.CharField(max_length=100, default="kenya")
-	gender 		= models.CharField(max_length=2, choices=GENDER)
-	religion 	= models.CharField(max_length=20, choices=RELIGION)
+	gender 		= models.CharField(max_length=2, choices=_GENDERS)
+	religion 	= models.CharField(max_length=20, choices=_RELIGIONS)
 
 
 	# Methods 
+
 	def __str__(self):
 		return str(self.nemis_no) + " - " + self.first_name + " - " + self.last_name
+
+
+class Subject(models.Model):
+
+	"""
+		Description
+		-------------
+			* A model which stores information about a subject
+			* Can be used to expand the database
+
+		Variables
+		----------
+			* _SUBJECTS = a tuple containing type of subjects
+
+		Fields
+		--------
+			* name = Characters
+			* subject_type = Character
+		
+		Methods
+		---------
+			* def __str__() = display
+
+	"""
+
+	_SUBJECT_TYPES = (
+			("ex", "Extracurriculum"),
+			("su", "Subject"),
+		)
+
+	name 	= models.CharField(max_length=50)
+	subject_type = models.CharField(max_length=2, choices=_SUBJECT_TYPES)
+
+
+	def __str__(self):
+		return self.name
+
+
+class Teacher(models.Model):
+
+	"""
+		
+		Descriptions
+		--------------
+			* A model which extends the django user model
+			* Handle Teachers informations
+
+		Variables
+		-----------
+			* _EMPLOYERS =  a tuple which hold the employer status either Government or private
+
+		Fields
+		--------
+			* user = foreign key to User model
+			* id_no = interger
+			* employee_id = integer (can be non for non government)
+			* phone_no = integer (must, unique)
+			* dob = date
+
+		Methods
+		---------
+			* def __str__() = display
+
+	"""
+
+
+	# Variables
+
+	_EMPLOYERS  = (
+			("tsc", "TSC"),
+			("pr", "Private"),
+		)
+
+
+	# Fields
+
+	user 		= models.ForeignKey(User, on_delete = models.CASCADE)
+
+	id_no 		= models.IntegerField()
+	employee_id = models.IntegerField(blank=True, null=True)
+	phone_no 	= models.IntegerField()
+
+	dob 		= models.DateField()
+
+
+	# Methods
+
+	def __str__(self):
+		return self.user.first_name + " - " + self.user.last_name
